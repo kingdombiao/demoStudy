ff7bda249dcca37cb510f442ecf541e4ac653494
kingdombiao <kingdombiao@163.com>
2019-08-25 18:37:56 +0800

    递归删除节点

--- zookeeperdemo/src/main/java/com/kingdombiao/zk/javaapi/ApiOperatorDemo.java
+++ zookeeperdemo/src/main/java/com/kingdombiao/zk/javaapi/ApiOperatorDemo.java
@@ -4,6 +4,7 @@ import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -23,13 +24,13 @@ public class ApiOperatorDemo implements Watcher {
         countDownLatch.await();
 
         //创建节点
-        // zooKeeper.create("/kingdombiao", "node".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+        //zooKeeper.create("/node", "node".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 
 
         //创建子节点
-        //zooKeeper.getData("/kingdombiao/kingdombiao10000000001", new ApiOperatorDemo(), STAT);
-       // zooKeeper.create("/kingdombiao/kingdombiao1", "node1".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
-        //TimeUnit.SECONDS.sleep(2);
+      /*  zooKeeper.getData("/node", new ApiOperatorDemo(), STAT);
+        zooKeeper.create("/node/node1", "node1".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        TimeUnit.SECONDS.sleep(2);*/
 
         //获取节点
         // byte[] data = zooKeeper.getData("/kingdombiao", new ApiOperatorDemo(), STAT);
@@ -41,15 +42,79 @@ public class ApiOperatorDemo implements Watcher {
         //TimeUnit.SECONDS.sleep(2);
 
         //删除节点
-        //zooKeeper.getData("/kingdombiao/kingdombiao10000000001", new ApiOperatorDemo(), STAT);
-        //zooKeeper.delete("/kingdombiao/kingdombiao10000000001", -1);
-        //TimeUnit.SECONDS.sleep(2);
+       /* zooKeeper.getData("/node/node1", new ApiOperatorDemo(), STAT);
+        zooKeeper.delete("/node/node1", -1);
+        TimeUnit.SECONDS.sleep(2);*/
 
         //修改子路径
-        zooKeeper.getData("/kingdombiao/kingdombiao10000000002", new ApiOperatorDemo(), STAT);
+       /* zooKeeper.getData("/kingdombiao/kingdombiao10000000002", new ApiOperatorDemo(), STAT);
         zooKeeper.setData("/kingdombiao/kingdombiao10000000002","kingdombiao2".getBytes(),-1);
-       TimeUnit.SECONDS.sleep(1);
+       TimeUnit.SECONDS.sleep(1);*/
 
+        createParentNode("/node1/node2/node3/node4");
+        deleteNode("/node1/node2");
+    }
+
+
+    /**
+     * 删除当前节点以及子节点
+     *
+     * @param path
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    private static void deleteNode(String path) throws KeeperException, InterruptedException {
+        String newPath = path;
+        if (!path.startsWith("/")) {
+            newPath = "/".concat(path);
+        }
+
+        if (path.endsWith("/")) {
+            newPath = newPath.substring(0, newPath.length() - 1);
+        }
+
+        //递归删除子节点
+        recursiveLookup(newPath);
+
+        // 再删除父节点
+        zooKeeper.delete(newPath,-1);
+
+    }
+
+    private static void recursiveLookup(String newPath) throws KeeperException, InterruptedException {
+        Stat stat = zooKeeper.exists(newPath, true);
+        if (stat != null) {
+            List<String> children = zooKeeper.getChildren(newPath, new ApiOperatorDemo());
+            for (String node : children) {
+                System.out.println(">>>>>> node: " + node);
+                recursiveLookup(String.format("%s/%s", newPath, node));
+                zooKeeper.delete(String.format("%s/%s", newPath, node), stat.getVersion());
+            }
+        }
+}
+
+
+    private static void createParentNode(String path) throws KeeperException, InterruptedException {
+
+        String newPath = path;
+        if (path.startsWith("/")) {
+            newPath = path.substring(1);
+        }
+
+        if (path.endsWith("/")) {
+            newPath = newPath.substring(0, newPath.length() - 1);
+        }
+        System.out.println("newPath is: " + newPath);
+        String[] splitPath = newPath.split("/");
+
+        //逐级创建节点
+        String subPath = "";
+        for (int i = 0; i < splitPath.length; i++) {
+            subPath = String.format("%s/%s", subPath, splitPath[i]);
+            if (zooKeeper.exists(subPath, new ApiOperatorDemo()) == null) {
+                zooKeeper.create(subPath, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            }
+        }
     }
 
 
@@ -69,8 +134,8 @@ public class ApiOperatorDemo implements Watcher {
                 }
             } else if (watchedEvent.getType() == Event.EventType.NodeCreated) {
                 try {
-                    System.out.println("节点创建路径：" + watchedEvent.getType()
-                            + ", 节点的值：" + zooKeeper.getData(watchedEvent.getPath(), true, STAT));
+                    System.out.println("节点创建路径：" + watchedEvent.getPath()
+                            + ", 节点的值：" + new String(zooKeeper.getData(watchedEvent.getPath(), true, STAT)));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
